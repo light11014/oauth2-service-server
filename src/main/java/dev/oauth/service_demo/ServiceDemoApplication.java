d46504b5cca1889ccce26d634ce3352d3c7dfa53
light11014 <dtq1014@naver.com>
2026-03-12 16:44:22 +0900

    first commit

---
+++ src/main/java/dev/oauth/service_demo/ServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.oauth.service_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/oauth/service_demo/ServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oauth.service_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
