7da859f0a2f71bfc35d26ea8a2faa7f1b0580b96
jooho2075 <parkjh2075@icloud.com>
2026-03-12 17:35:03 +0900

    SecurityConfig.java 재시도

---
+++ oauth2-service-server/src/main/java/dev/oauth/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package dev.oauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(proxyBeanMethods = false)
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .anyRequest()
+                                .authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("user")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+
+    }
+}
---
+++ oauth2-service-server/src/test/java/dev/oauth/ServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
